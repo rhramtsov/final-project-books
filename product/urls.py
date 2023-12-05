61eb3655b3c9a4e789d3d4b1fcdaacc38e26aa00
rita <r.hramtsov@gmail.com>
2023-12-05 16:56:29 +0200

    json11

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import include, path  
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 urlpatterns = [
     # product - for list of products and create product.
+    path('', views.index, name='homepage'),
     path('product', views.products, name="products"), 
     path('product/<id>', views.product_detail, name="product_detail"), 
     path('category', views.categories, name="categories"),    
