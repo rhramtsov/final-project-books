de2ea6169a46822d51b68e4c9e78cde432e5a109
rita <r.hramtsov@gmail.com>
2023-12-06 14:47:31 +0200

    urls

--- product/urls.py
+++ product/urls.py
@@ -10,8 +10,14 @@ from django.conf.urls.static import static
 urlpatterns = [
     # product - for list of products and create product.
     path('', views.index, name='homepage'),
-    path('product', views.products, name="products"), 
+    path('All Books', views.products, name="all books"), 
+    path('Art Books', views.products, name="art books"), 
+    path('Fiction Books', views.products, name="fiction books"), 
+    path('Children Books', views.products, name="children books"), 
     path('product/<id>', views.product_detail, name="product_detail"), 
-    path('category', views.categories, name="categories"),    
+    # path('category', views.categories, name="categories"),    
     path('cartitems', views.cartitems, name="cartitems"),    
-]
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    ]
