28c6d3b9954dbc96e7d1748cb8355245106e4b72
adamh0807 <adamhavakook@gmail.com>
2023-12-09 13:00:58 +0200

    Merge upstream/main into my branch

--- product/models.py
+++ product/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=False)
     price = models.DecimalField(max_digits=16, decimal_places=2)
     stock = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
 
     def __str__(self):
         return f'{self.name}'
