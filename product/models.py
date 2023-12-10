ecf72c1cf84c11ecff9d2ea9e3d21b195ce5199b
rita <r.hramtsov@gmail.com>
2023-12-10 19:25:10 +0200

    rr

--- product/models.py
+++ product/models.py
@@ -15,11 +15,12 @@ class MyUser(AbstractUser):
   
 class Product(models.Model):
     
-    category = models.ForeignKey('Category',on_delete=models.CASCADE, related_name='products')
+    categories = models.ManyToManyField('Category', related_name='products')
     name = models.CharField(max_length=100, null=False)
     price = models.DecimalField(max_digits=16, decimal_places=2)
     stock = models.IntegerField(default=0)
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)    
+    
     def __str__(self):
         return f'{self.name}'
 
--- product_proj/settings.py
+++ product_proj/settings.py
@@ -176,13 +176,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_FILE_PATH = "sent_emails"
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'theonlineartgalleryhl@gmail.com'
-# EMAIL_HOST_PASSWORD = 'dflhabzpaqlpfsjq'
-# DEFAULT_FROM_EMAIL = 'theonlineartgalleryhl@gmail.com'
-# SERVER_EMAIL = 'theonlineartgalleryhl@gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_FILE_PATH = "sent_emails"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'theonlineartgalleryhl@gmail.com'
+EMAIL_HOST_PASSWORD = 'dflhabzpaqlpfsjq'
+DEFAULT_FROM_EMAIL = 'theonlineartgalleryhl@gmail.com'
+SERVER_EMAIL = 'theonlineartgalleryhl@gmail.com'
 
